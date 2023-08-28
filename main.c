42258b74a69ff813de0d9bb6c01e280d7d46de30
mamesser <mamesser@student.42wolfsburg.de>
2023-08-28 14:22:49 +0200

    init var struct and testing eat,sleep,think cycle

--- main.c
+++ main.c
@@ -6,63 +6,97 @@
 /*   By: mamesser <mamesser@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 16:58:54 by mamesser          #+#    #+#             */
-/*   Updated: 2023/08/27 18:20:28 by mamesser         ###   ########.fr       */
+/*   Updated: 2023/08/28 14:20:47 by mamesser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./includes/philo.h"
 
+t_vars *init_vars(int num_philo) // add args for time to die/eat/sleep
+{
+	int		i;
+	t_vars	*vars;
+
+	i = 0;
+	vars = malloc(num_philo * sizeof(*vars) + 1000);
+	if (!vars)
+		return (NULL);
+	while (i < num_philo)
+	{
+		(vars[i]).time_to_die = 300;
+		(vars[i]).time_to_eat = 10;
+		(vars[i]).time_to_sleep = 50;
+		(vars[i]).philo = malloc(sizeof(t_philo));
+		(vars[i]).philo->id = i + 1;
+		i++;
+	}
+	return (vars);
+}
+
+
 void	*philosopher_dines(void *arg) // probably philo id as argument; also needs info about time to eat etc.
 {
 	t_vars *vars = (t_vars *)arg;
-	
-	printf("philo id: %d\n", vars->philo->id);
-	
-	// while (1) // also stop if all philos have eaten at least times (provided as argument)
-	// {
-	// 	// if philo id is odd, take up right fork first
-	// 	// check if fork is logged, if not, take the fork
-	// 	printf("TIMESTAMP: Philosopher %d has taken a fork\n", philo_id);
-		
-	// 	// if philo id is even, take up left fork first
-	// 	// check if fork is logged, if not, take the fork
 
-	// 	// if philo has two forks taken up, philo eats for X milliseconds (provided as argument); use usleep?
-	// 	printf("TIMESTAMP: Philosopher %d is eating\n", philo_id);
+	while (1) // also stop if all philos have eaten at least times (provided as argument)
+	{
+		if (vars->philo->id % 2 != 0)
+		{
+			// check if fork is logged, if not, take the left fork
+			printf("TIMESTAMP: Philosopher %d has taken the left fork\n", vars->philo->id);
+			// check if fork is logged, if not, take the right fork
+			printf("TIMESTAMP: Philosopher %d has taken the right fork\n", vars->philo->id);
+			
+		}
+		else if (vars->philo->id % 2 == 0)
+		{
+			// check if fork is logged, if not, take the right fork
+			printf("TIMESTAMP: Philosopher %d has taken the right fork\n", vars->philo->id);
+			// check if fork is logged, if not, take the left fork
+			printf("TIMESTAMP: Philosopher %d has taken the left fork\n", vars->philo->id);
+		}
+		// if philo has two forks taken up, philo eats for X milliseconds (provided as argument); use usleep?
+		printf("TIMESTAMP: Philosopher %d is eating\n", vars->philo->id);
+		usleep(5000000); // takes microseconds 1000000ms = 1sec
 
-	// 	// once philo has finished eating, unlock the used forks
+		// once philo has finished eating, unlock the used forks
 
-	// 	//start sleeping for X milliseconds (provided as argument); use usleep?
-	// 	printf("TIMESTAMP: Philosopher %d is sleeping\n", philo_id);
+		//start sleeping for X milliseconds (provided as argument); use usleep?
+		printf("TIMESTAMP: Philosopher %d is sleeping\n", vars->philo->id);
+		usleep(5000000);
 
-	// 	printf("TIMESTAMP: Philosopher %d is thinking\n", philo_id);
-	// }
+		printf("TIMESTAMP: Philosopher %d is thinking\n", vars->philo->id);
+	}
 	return (NULL);
 }
 
 
 int	main(int argc, char **argv)
 {
-	pthread_t	newthread[10]; // array of threads needs to be dynamic (bc dependent on input)
-	t_vars		vars;
+	pthread_t	*newthread;
+	t_vars		*vars;
+	int			num_philo = 2;
 	int			i;
 
-	i = 1;
-	vars.time_to_die = 300;
-	vars.time_to_eat = 10;
-	vars.time_to_sleep = 50;
-	while (i < 10)
+	i = 0;
+	newthread = malloc(num_philo * sizeof(*newthread));
+	if (!newthread)
+		return (1);
+	vars = init_vars(num_philo);
+	if (!vars)
+		return (1);
+	while (i < num_philo)
 	{
-		vars.philo->id = i;
 		// create threads for each philosopher (number is given by argv[1]; struct for each philo?)
-		if (pthread_create(&newthread[i], NULL, philosopher_dines, &vars))
+		if (pthread_create(&newthread[i], NULL, philosopher_dines, &vars[i]))
 			return (1);
 		i++;
 	}
-	i = 1;
+	i = 0;
 	while (i < 10)
 	{
 		if (pthread_join(newthread[i], NULL))
 			return (1);
+		i++;
 	}
 }
